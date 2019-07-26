a27192f33a373d710835b9f525209303481923d2
mapulu <46723804+mapulu@users.noreply.github.com>
2019-07-26 21:26:19 +0200

    More german translations and more
    Added also missing icons and insights vor custom capabilities

--- drivers/700793/device.js
+++ drivers/700793/device.js
@@ -5,6 +5,8 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 
 class P700793 extends ZwaveDevice {
 	onMeshInit() {
+		this.enableDebug();
+		this.printNode();
 		this.registerCapability('onoff', 'SWITCH_BINARY');
 		this.registerCapability('measure_power', 'METER');
 		this.registerCapability('meter_power', 'METER');
