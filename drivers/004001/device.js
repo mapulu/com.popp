f6135c8048f10141dac220de5e93c8feaf75777b
mapulu <46723804+mapulu@users.noreply.github.com>
2019-07-27 18:55:31 +0200

    Update device.js
    Should use SENSOR_ALARM or SENSOR_BINARY for smoke alarm

--- drivers/004001/device.js
+++ drivers/004001/device.js
@@ -6,8 +6,10 @@ const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
 class P004001 extends ZwaveDevice {
 	onMeshInit() {
 		this.registerCapability('onoff', 'SWITCH_BINARY');
-		this.registerCapability('alarm_smoke', 'ALARM');
-		this.registerCapability('alarm_tamper', 'ALARM');
+		this.registerCapability('alarm_smoke', 'SENSOR_ALARM');
+		this.registerCapability('alarm_smoke', 'SENSOR_BINARY');
+		this.registerCapability('alarm_tamper', 'SENSOR_ALARM');
+		this.registerCapability('alarm_tamper', 'SENSOR_BINARY');
 		this.registerCapability('alarm_battery', 'BATTERY');
 		this.registerCapability('measure_battery', 'BATTERY');
 	}
